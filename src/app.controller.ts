6b2ebc3f309ebaa217263bf4a495ab24e7926dc1
Dexter <dexter@fedora.home>
2022-11-02 10:26:10 +0100

    cleaning

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,13 @@
-import { HttpService } from '@nestjs/axios';
 import { Controller, Get, UseGuards, Request, Post } from '@nestjs/common';
 import { AuthGuard } from 'nest-keycloak-connect';
 
 @Controller()
 export class AppController {
-  constructor(private readonly httpService: HttpService) {}
+  constructor() {}
 
   @Get()
   @UseGuards(AuthGuard)
   getHello(): string {
-    // this.httpService.get('http://127.0.0.1:8080/realms/demo').subscribe((res) => {
-    //   console.log(res);
-    // })
     return "Hello";
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -11,8 +11,7 @@ import { Neo4jConfig } from './neo4j/neo4j-config.interface';
 import { upperDirectiveTransformer } from './graphql/directives/upper-case.directive';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { join } from 'path';
-import { KeycloakConnectModule, PolicyEnforcementMode, TokenValidation } from 'nest-keycloak-connect';
-import { HttpModule } from '@nestjs/axios';
+import { KeycloakConnectModule, KeycloakConnectOptions, PolicyEnforcementMode, TokenValidation } from 'nest-keycloak-connect';
 
 @Module({
   imports: [
@@ -36,7 +35,7 @@ import { HttpModule } from '@nestjs/axios';
     ConfigModule.forRoot({ isGlobal: true }),
     Neo4jModule.forRootAsync({
       imports: [ ConfigModule ],
-      inject: [ ConfigService, ],
+      inject: [ ConfigService ],
       useFactory: (configService: ConfigService) : Neo4jConfig => ({
         scheme: configService.get('NEO4J_SCHEME'),
         host: configService.get('NEO4J_HOSTNAME'),
@@ -46,22 +45,18 @@ import { HttpModule } from '@nestjs/axios';
         database: configService.get('NEO4J_DATABASE'),
       })
     }),
-    KeycloakConnectModule.register({
-      authServerUrl: 'http://localhost:8080',
-      realm: 'demo',
-      clientId: 'demo-api',
-      secret: 'jsKOzC1LLfy8UIRIdktiXKLSxI185TKn',
-      bearerOnly: true,
-      realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1S6xZg3nuQoh04LUS+MIiCusPHtQ73/PUwN6k3o2QSXeBn79K0QIQH7LMECcZ2YhS23AXTjX71hLWBzi498JWKKJjJ8ZpoH1M9lJKx3pDfzeqQD66eq6E25lYG65hNDqzCQABvnw7AsVIY8GMPMrc+o+qfrrGigge5krciX+dD0KY9lo1v1otyZB8UT1V9geEGaWepfpuz73Qc2dU95CH/mPMpatjB1s1WJDzJZWPFQtXmLdFCjn5LBWked6CcMsrZ5cbkooLxbT+Ur0RvYAeDxwxlTX3oqm63uxZgXgk/WiT6i9IWZKniQ7TrXw9ijiblqKm/Ha3kCjx0ApjxCxpQIDAQAB",
-      policyEnforcement: PolicyEnforcementMode.PERMISSIVE, // optional
-      tokenValidation: TokenValidation.ONLINE, // optional
-      // logLevels: ['verbose'],
-      // useNestLogger: false,
-    }),
-    HttpModule.register({
-      timeout: 5000,
-      maxRedirects: 5,
-      headers: { 'Access-Control-Allow-Origin': '*' }
+    KeycloakConnectModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: (configService: ConfigService) : KeycloakConnectOptions => ({
+        authServerUrl: configService.get('KEYCLOAK_AUTH_SERVER_URL'),
+        realm: configService.get('KEYCLOAK_REALM'),
+        clientId: configService.get('KEYCLOAK_CLIENT_ID'),
+        secret: configService.get('KEYCLOAK_SECRET'),
+        realmPublicKey : configService.get('KEYCLOAK_REALM_PUBLIC_KEY'),
+        policyEnforcement: PolicyEnforcementMode.PERMISSIVE,
+        tokenValidation: TokenValidation.ONLINE,
+      }),
     })
   ],
   controllers: [AppController],
