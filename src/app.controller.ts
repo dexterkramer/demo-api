b66ebd15828ce749606eda9316cc38c9439d4bae
Benoit Sauveton <bsauveton@gmail.com>
2023-11-22 15:05:52 +0100

    wip file upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, UseGuards, Request, Post } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from 'nest-keycloak-connect';
 
 @Controller()
@@ -9,5 +10,11 @@ export class AppController {
   @UseGuards(AuthGuard)
   getHello(): string {
     return "Hello";
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
